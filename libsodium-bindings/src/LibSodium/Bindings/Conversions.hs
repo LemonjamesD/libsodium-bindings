b40a4deb848c39dd6fce942cd12f301195ad3100
LemonjamesD <lemon@lemonjamesd.com>
2023-10-13 22:18:37 -0400

    Added base64 to bin

--- libsodium-bindings/src/LibSodium/Bindings/Conversions.hs
+++ libsodium-bindings/src/LibSodium/Bindings/Conversions.hs
@@ -40,3 +40,37 @@ foreign import capi "sodium.h sodium_bin2base64"
     -- ^ NOTE: This is `const int` in sodium
     -> Ptr CChar
     -- ^ The result of converting bin to base64
+
+-- | Converts a base64 number to binary
+--
+-- /See:/ [base64-encoding-decoding](https://libsodium.gitbook.io/doc/helpers#base64-encoding-decoding)
+foreign import capi "sodium.h sodium_base642bin"
+  sodiumBase642Bin
+    :: Ptr CUChar
+    -- ^ Place to store the resulting binary number
+    -- ^ NOTE: This is `unsigned char *const` in sodium
+    -> CSize
+    -- ^ Amount of bytes allowed to write to the storage place of the binary number
+    -- ^ NOTE: This is `const size_t` in sodium
+    -> ConstPtr CChar
+    -- ^ Base64 number to decode
+    -- ^ NOTE: This is `const char *const` in sodium
+    -> CSize
+    -- ^ Length of bytes to decode into a binary number
+    -- ^ NOTE: This is `const size_t` in sodium
+    -> ConstPtr CChar
+    -- ^ TODO
+    -- ^ NOTE: This is `const char *const` in sodium
+    -> Ptr CSize
+    -- ^ TODO
+    -- ^ NOTE: This is `size_t *const` in sodium
+    -> Ptr ConstPtr CChar
+    -- ^ TODO
+    -- ^ NOTE: This is `const char **const` in sodium
+    -> CInt
+    -- ^ Variant type of the base64 number
+    -- ^ NOTE: This is `const int` in sodium
+    -> CInt
+    -- ^ Error code, 0 on success,
+    -- ^ -1 if more than bin_maxlen bytes would be required,
+    -- ^ or it the string couldn't be fully parsed
